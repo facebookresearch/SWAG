c19d62e641455ce78191cf8cfdf044a970e87fe0
Mannat Singh <mannatsingh@fb.com>
2022-01-19 12:04:06 -0800

    Use bicubic for eval

--- in1k_eval.py
+++ in1k_eval.py
@@ -140,15 +140,15 @@ def main_worker(gpu, ngpus_per_node, args):
 
     # Data loading code
     valdir = os.path.join(args.data, 'val')
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
 
     val_loader = torch.utils.data.DataLoader(
         datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Resize(args.resolution),
+            transforms.Resize(args.resolution,
+                interpolation=transforms.InterpolationMode.BICUBIC),
             transforms.CenterCrop(args.resolution),
             transforms.ToTensor(),
-            normalize,
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225]),
         ])),
         batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
