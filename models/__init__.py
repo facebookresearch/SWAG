d3a2e2d9d7ec99b517964f1f35322e4cb99027f2
Mannat Singh <mannatsingh@fb.com>
2022-01-19 13:22:21 -0800

    More clean up

--- models/__init__.py
+++ models/__init__.py
@@ -19,7 +19,9 @@ class ModelCheckpoints(Enum):
     regnety_32gf = ""
     regnety_32gf_in1k = "https://dl.fbaipublicfiles.com/SWAG/regnety_32gf_in1k.torch"
     regnety_128gf = "https://dl.fbaipublicfiles.com/SWAG/regnety_128gf_10b.torch"
-    regnety_128gf_in1k = "https://dl.fbaipublicfiles.com/SWAG/regnety_128gf_10b_in_ft.torch"
+    regnety_128gf_in1k = (
+        "https://dl.fbaipublicfiles.com/SWAG/regnety_128gf_10b_in_ft.torch"
+    )
     vit_b16 = ""
     vit_b16_in1k = "https://dl.fbaipublicfiles.com/SWAG/vit_b16_in1k.torch"
     vit_l16 = ""
@@ -35,185 +37,146 @@ def build_model(
     cls: type,
     checkpoint_path: str,
     pretrained: bool = True,
-    map_location: Any = None,
     progress: bool = True,
     **kwargs
 ):
     model = cls(**kwargs)
     if pretrained:
-        checkpoint = load_state_dict_from_url(
-            checkpoint_path, map_location=map_location, progress=progress
-        )
+        checkpoint = load_state_dict_from_url(checkpoint_path, progress=progress)
         model.load_state_dict(checkpoint)
     return model
 
 
-def regnety_16gf(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def regnety_16gf(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         RegNetY16gf,
         ModelCheckpoints.regnety_16gf.value,
         num_classes=None,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def regnety_16gf_in1k(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def regnety_16gf_in1k(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         RegNetY16gf,
         ModelCheckpoints.regnety_16gf_in1k.value,
         num_classes=IN1K_CLASSES,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def regnety_32gf(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def regnety_32gf(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         RegNetY32gf,
         ModelCheckpoints.regnety_32gf.value,
         num_classes=None,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def regnety_32gf_in1k(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def regnety_32gf_in1k(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         RegNetY32gf,
         ModelCheckpoints.regnety_32gf_in1k.value,
         num_classes=IN1K_CLASSES,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def regnety_128gf(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def regnety_128gf(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         RegNetY128gf,
         ModelCheckpoints.regnety_128gf.value,
         num_classes=None,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def regnety_128gf_in1k(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def regnety_128gf_in1k(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         RegNetY128gf,
         ModelCheckpoints.regnety_128gf_in1k.value,
         num_classes=IN1K_CLASSES,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def vit_b16(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def vit_b16(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         ViTB16,
         ModelCheckpoints.vit_b16.value,
         num_classes=None,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def vit_b16_in1k(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def vit_b16_in1k(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         ViTB16,
         ModelCheckpoints.vit_b16_in1k.value,
         image_size=384,
         num_classes=IN1K_CLASSES,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def vit_l16(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def vit_l16(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         ViTL16,
         ModelCheckpoints.vit_l16.value,
         num_classes=None,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def vit_l16_in1k(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def vit_l16_in1k(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         ViTL16,
         ModelCheckpoints.vit_l16_in1k.value,
         image_size=512,
         num_classes=IN1K_CLASSES,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def vit_h14(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def vit_h14(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         ViTH14,
         ModelCheckpoints.vit_h14.value,
         num_classes=None,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
 
 
-def vit_h14_in1k(
-    pretrained: bool = True, map_location: Any = None, progress: bool = True, **kwargs
-):
+def vit_h14_in1k(pretrained: bool = True, progress: bool = True, **kwargs):
     return build_model(
         ViTH14,
         ModelCheckpoints.vit_h14_in1k.value,
         image_size=518,
         num_classes=IN1K_CLASSES,
         pretrained=pretrained,
-        map_location=map_location,
         progress=progress,
         **kwargs
     )
